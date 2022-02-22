db115047ac7038723472ca2cb8f89b9fcb67337a
matthew chen <matthewchen@ucla.edu>
2022-02-22 16:07:02 -0500

    max subarray

--- samsung/53-max-subarray.js
+++ samsung/53-max-subarray.js
@@ -3,7 +3,7 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
+ var maxSubArrayS1 = function(nums) {
   if (nums.length === 1) return nums[0];
   
   let maxSum = nums[0];
@@ -15,6 +15,26 @@
           maxSum = Math.max(currentSum, maxSum);
           if (nums[j] > 0 && currentSum < 0) break;
       }
+  }
+  
+  return maxSum;
+};
+
+/**
+ * dynamic programming approach
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArrayS2 = function(nums) {
+  if (nums.length === 1) return nums[0];
+  
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+  
+  for (let i = 1; i < nums.length; i++) {
+      let num = nums[i];
+      currentSum = Math.max(num, currentSum + num);
+      maxSum = Math.max(maxSum, currentSum);
   }
   
   return maxSum;
