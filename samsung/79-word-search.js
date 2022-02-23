1e8ca26c8565f704f3d598189fb7a7ab7afcdb3c
matthew chen <matthewchen@ucla.edu>
2022-02-22 19:22:30 -0500

    word search

---
+++ samsung/79-word-search.js
@@ -0,0 +1,30 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+ var exist = function(board, word) {
+  let dfs = (board, i, j, word, count=0) => {
+      if(count===word.length) return true;
+      if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j]!==word[count]) {
+          return false;
+      }
+      let temp = board[i][j];
+      board[i][j] = '';
+      let found = dfs(board, i-1, j, word, count+1) 
+          || dfs(board, i+1, j, word, count+1) 
+          || dfs(board, i, j-1, word, count+1) 
+          || dfs(board, i, j+1, word, count+1);
+      board[i][j] = temp;
+      return found;
+  }
+  
+  for(let i = 0; i<board.length; i++){
+      for(let j = 0; j<board[0].length; j++){
+          if(board[i][j]===word[0] && dfs(board, i, j, word)){
+              return true;
+          }
+      }
+  }
+  return false;
+};
